12799dd24f1f63f97f9451ce7023ccf2e76d9d04
NandaDDA <nandadulal2101@gmail.com>
2023-02-11 10:36:50 +0000

    amendments to engine

--- models/amenity.py
+++ models/amenity.py
@@ -1,12 +1,7 @@
-#!/usr/bin/python3
-"""Defines the Amenity class."""
+#!/usr/bin/python:wq!
+""" State Module for HBNB project """
 from models.base_model import BaseModel
 
+
 class Amenity(BaseModel):
-    """Represent an amenity.
-
-    Attributes:
-        name (str): The name of the amenity.
-    """
-
     name = ""
